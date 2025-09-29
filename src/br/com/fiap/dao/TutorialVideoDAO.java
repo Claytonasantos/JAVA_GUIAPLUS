cedf82a63d861b449ec04d601af59763f99b5cbc
Claytonasantos <claytonalves2579@gmail.com>
2025-09-29 11:31:58 -0300

    Adicionando mais arquivos no crud

---
+++ src/br/com/fiap/dao/TutorialVideoDAO.java
@@ -0,0 +1,53 @@
+package br.com.fiap.dao;
+
+import br.com.fiap.beans.CategoriaTutorial;
+import br.com.fiap.conexoes.ConexaoFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class TutorialDAO {
+    public Connection minhaConexao;
+
+    public TutorialDAO() throws SQLException, ClassNotFoundException {
+        this.minhaConexao = new ConexaoFactory().conexao();
+    }
+
+    //insert
+    public String inserirTutorial(Categor categoriaTutorial) throws SQLException {
+        PreparedStatement stmt = minhaConexao.prepareStatement
+                ("Insert into categoria_tutorial (nome_categoria, desc_categoria) values (?, ?)");
+        stmt.setString(1,categoriaTutorial.getNome());
+        stmt.setString(2,categoriaTutorial.getDescricaoCategoriaTutorial());
+
+        stmt.execute();
+        stmt.close();
+
+        return "Categoria inserida com sucesso!";
+    }
+
+    //Deletar
+    public String deletarCategoria(Integer id_categoria) throws SQLException {
+        PreparedStatement stmt =
+                minhaConexao.prepareStatement("Delete from categoria_tutorial where id_categoria = ?");
+        stmt.setInt(1, id_categoria);
+        stmt.execute();
+        stmt.close();
+
+        return "Categoria deletada com sucesso!";
+    }
+
+    //Update
+    public String atualizarCategoria (CategoriaTutorial categoriaTutorial) throws SQLException {
+        PreparedStatement stmt = minhaConexao.prepareStatement("Update categoria_tutorial set nome_categoria = ?, desc_categoria = ? where id_categoria = ?");
+        stmt.setString(1, categoriaTutorial.getNome());
+        stmt.setString(2, categoriaTutorial.getDescricaoCategoriaTutorial());
+        stmt.setInt(4,categoriaTutorial.getId_categoria());
+
+        stmt.executeUpdate();
+        stmt.close();
+
+        return "Categoria atualizada com sucesso!";
+    }
+}
